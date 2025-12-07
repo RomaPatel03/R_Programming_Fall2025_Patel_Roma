c1a56dabe48508973c4ad155bde918eec2a878be
RomaPatel03 <romapatel1@usf.edu>
2025-12-06 19:04:02 -0500

    Delete Numeric.R

--- Numeric.R
+++
@@ -1,7 +0,0 @@
-normalize_col <- function(col) {
-       if(!is.numeric(col)) stop("Input must be numeric")
-       if(min(col) == max(col)) return(rep(0, length(col)))  # handle single-value vector
-       (col - min(col)) / (max(col) - min(col))
-        }
-norm <- normalize_col(cleaned$a)
-norm
