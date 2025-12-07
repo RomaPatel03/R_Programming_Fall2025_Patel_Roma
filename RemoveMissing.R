f3cb5def3d2361aed7242745925ded5d583fa8bc
RomaPatel03 <romapatel1@usf.edu>
2025-12-06 19:03:42 -0500

    Delete RemoveMissing.R

--- RemoveMissing.R
+++
@@ -1,9 +0,0 @@
-# remove_missing_rows: Removes rows with any missing values
-remove_missing_rows <- function(df) {
-  df[complete.cases(df), ]
-}
-data <- data.frame(a = c(1, NA, 3), b = c(NA, 2, 3))
-cleaned <- remove_missing_rows(data)
-cleaned
-
-
